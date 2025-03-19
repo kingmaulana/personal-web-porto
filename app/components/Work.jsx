6030ecf372d6519cb12a39faa3bbabcad79bf292
kingmaulana <akbarquipper68@gmail.com>
2025-03-19 12:57:57 +0700

    change color at hover effect

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -20,7 +20,7 @@ function Work({isDarkMode}) {
                     <p className='text-sm text-gray-700'>{project.description}</p>
                 </div>
 
-                <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition'>
+                <div className='border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-pink-600 transition'>
                     <a href={project.link} target='_blank'><Image src={assets.send_icon} alt='send icon' className='w-5'/></a> 
                 </div>
                 </div>
